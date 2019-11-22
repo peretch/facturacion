d87db63d1d9b60f9e18f677b153908df8acb5fcf
peretch <sebastian@peretch.com>
2019-11-22 11:53:10 -0300

    Corrijo nombre de variable en vistas

--- resources/views/comprobantes/detalle.blade.php
+++ resources/views/comprobantes/detalle.blade.php
@@ -6,7 +6,7 @@
 		<div class="col-md-12">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					<h4>Detalle de factura</h4>
+					<h4>Detalle de comprobante</h4>
 				</div>                
 				<div class="panel-body">
 					<ul class="list-inline">
@@ -16,12 +16,12 @@
 							</a>
 						</li>
 						<li>
-							<a href="/facturas" class="link_ruta">
-								Facturas &nbsp; &nbsp;<i class="fa fa-chevron-right" aria-hidden="true"></i>
+							<a href="/comprobantes" class="link_ruta">
+								comprobantes &nbsp; &nbsp;<i class="fa fa-chevron-right" aria-hidden="true"></i>
 							</a>
 						</li>
 						<li>
-							<a href="/facturas/detalle/{{$factura->id}}" class="link_ruta">
+							<a href="/comprobantes/detalle/{{$comprobante->id}}" class="link_ruta">
 								Detalle
 							</a>
 						</li>
@@ -29,58 +29,58 @@
 					@include('partials.menu_productos')                    
 					<div class="row">
 						<div class="col-md-4">                            
-							<legend>Datos de la factura</legend>                               
+							<legend>Datos de la comprobante</legend>                               
 							<table class="table">
 								<tr>
 									<td>Fecha de emisión</td>
-									@if($factura->fechaEmision)
-										<td>{{ $factura->fechaEmision }}</td>
+									@if($comprobante->fechaEmision)
+										<td>{{ $comprobante->fechaEmision }}</td>
 									@else
 										<td style="color: #aaa;">Sin especificar</td>
 									@endif
 								</tr>
 								<tr>
 									<td>Serie</td>
-									@if($factura->serie)
-										<td>{{ $factura->serie }}</td>
+									@if($comprobante->serie)
+										<td>{{ $comprobante->serie }}</td>
 									@else
 										<td style="color: #aaa;">Sin especificar</td>
 									@endif
 								</tr>
 								<tr>
 									<td>Número</td>
-									@if($factura->serie)
-										<td>{{ $factura->numero }}</td>
+									@if($comprobante->serie)
+										<td>{{ $comprobante->numero }}</td>
 									@else
 										<td style="color: #aaa;">Sin especificar</td>
 									@endif
 								</tr>
 								<tr>
 									<td>Cliente</td>
-									@if($factura->cliente)
-										<td><a href="/clientes/detalle/{{ $factura->cliente->id }}">{{ $factura->cliente->nombre }} {{ $factura->cliente->apellido }}</a></td>
+									@if($comprobante->cliente)
+										<td><a href="/clientes/detalle/{{ $comprobante->cliente->id }}">{{ $comprobante->cliente->nombre }} {{ $comprobante->cliente->apellido }}</a></td>
 									@else
-									   <td>{{ $factura->nombreCliente }}</td>
+									   <td>{{ $comprobante->nombreCliente }}</td>
 									@endif
 								</tr>
 								<tr>
 									<td>RUT</td>
-									@if($factura->serie)
-										<td>{{ $factura->rut }}</td>
+									@if($comprobante->serie)
+										<td>{{ $comprobante->rut }}</td>
 									@else
 										<td>N/A</td>
 									@endif
 								</tr>
 								<tr>
 									<td>Dirección</td>
-									@if($factura->direccion)
-										<td>{{ $factura->direccion }}</td>
+									@if($comprobante->direccion)
+										<td>{{ $comprobante->direccion }}</td>
 									@else
 										<td style="color: #aaa;">Sin especificar</td>
 									@endif
 								</tr>
 							</table>
-							<a href="/facturas/imprimir/{{$factura->id}}" target="_blank" class="btn btn-block btn-primary">Imprimir <i class="fa fa-print" aria-hidden="true"></i></a>
+							<a href="/comprobantes/imprimir/{{$comprobante->id}}" target="_blank" class="btn btn-block btn-primary">Imprimir <i class="fa fa-print" aria-hidden="true"></i></a>
 						</div>
 						<div class="col-md-8">                            
 							<legend>
@@ -90,7 +90,7 @@
 								</div>                                    
 							</div>
 							</legend>
-							<div class="col-md-12 pre-scrollable div-detalle-factura">
+							<div class="col-md-12 pre-scrollable div-detalle-comprobante">
 								<table width="100%" class="table-condensed table-striped table-bordered">
 									<thead>
 										<tr>
@@ -104,7 +104,7 @@
 										</tr>
 									</thead> 
 									<tbody id="tablaProductos">
-										@foreach($factura->lineasProducto as $l)
+										@foreach($comprobante->lineasProducto as $l)
 										<tr>
 											<td><a href="/productos/detalle/{{ $l->producto->codigo }}">{{ $l->producto->codigo }}</a></td>
 											<td>{{ $l->producto->nombre }}</td>
@@ -125,15 +125,15 @@
 									<thead id="tablaResumen">
 										<tr>
 											<td>SUB-TOTAL</td>
-											<td>{{ number_format($factura->subTotal, 0, '.', ',') }}</td>
+											<td>{{ number_format($comprobante->subTotal, 0, '.', ',') }}</td>
 										</tr>
 										<tr>
 											<td>IVA</td>
-											<td>{{ number_format($factura->impuestos, 0, '.', ',') }}</td>
+											<td>{{ number_format($comprobante->impuestos, 0, '.', ',') }}</td>
 										</tr>
 										<tr>
 											<td>TOTAL</td>
-											<td>{{ number_format($factura->total, 0, '.', ',') }}</td>
+											<td>{{ number_format($comprobante->total, 0, '.', ',') }}</td>
 										</tr>
 									</thead>
 								</table>
